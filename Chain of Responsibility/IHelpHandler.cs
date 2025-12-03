4b9aa8c75bbc28c3ffd3802ad30d82b163d5b438
Max <makspolzikov06@gmail.com>
2025-12-03 08:12:06 +0200

    Вынес Topic в Chain

--- Chain of Responsibility/IHelpHandler.cs
+++ Chain of Responsibility/IHelpHandler.cs
@@ -1,12 +1,6 @@
-﻿namespace ChainOfResponsibility
+﻿using Topics;
+namespace ChainOfResponsibilit7y
 {
-    public enum Topic
-    {
-        NO_HELP_TOPIC = -1,
-        PRINT_TOPIC = 1,
-        PAPER_ORIENTATION_TOPIC = 2,
-        APPLICATION_TOPIC = 3
-    }
     public interface IHelpHandler
     {
         bool HasHelp();
---
+++ Chain of Responsibility/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topics
+    public enum Topic
+    {
+        NO_HELP_TOPIC = -1,
+        PRINT_TOPIC = 1,
+        PAPER_ORIENTATION_TOPIC = 2,
+        APPLICATION_TOPIC = 3
+    }
+}
