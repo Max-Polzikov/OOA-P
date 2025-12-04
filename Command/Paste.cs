23e86014e87ee68966f1df00a5d0cda7d6dfd4dd
Max <makspolzikov06@gmail.com>
2025-12-04 19:45:33 +0200

    Command

--- Command/Paste.cs
+++ Command/Paste.cs
@@ -8,6 +8,7 @@ namespace Command
     public class PasteCommand : ICommand
     {
         private readonly Document _document;
+        private string _pastedText = "";
 
         public PasteCommand(Document document)
         {
@@ -16,7 +17,14 @@ namespace Command
 
         public void Execute()
         {
+            _pastedText = _document.Clipboard;
             _document.Paste();
+        }
+
+        public void Undo()
+        {
+            _document.Remove(_pastedText);
+            Console.WriteLine($"Undo Paste: удалено '{_pastedText}'");
         }
     }
 }
